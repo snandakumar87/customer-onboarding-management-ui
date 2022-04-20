699cdfd48da44776cf8732fd7036c6551a628354
snandakumar87 <snandaku@redhat.com>
2022-04-19 22:35:31 -0400

    customer ui for onboarding

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -6,10 +6,10 @@ if (!!window.EventSource) {
 
         data = JSON.parse(event.data);
 
-        var jsonData = JSON.parse(data);
+
 
         var row = '<tr><td>' + data.requestId+ '</td><td>' + data.dueDiligence + '</td><td>' + data.Documents + '</td><td>' +
-        + JSON.stringify(jsonData) + '</td>';
+        + JSON.stringify(data,null,10) + '</td>';
 
 
 
